dfd79dc3f249b6677f8e6a3469643eae81f76994
Ben Liu <ben.liu@ikala.ai>
2024-12-21 15:15:53 +0800

    wip

--- demo.go
+++ demo.go
@@ -1,17 +1,18 @@
-// Package plugindemo a demo plugin.
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
-	"text/template"
 
 	"github.com/http-wasm/http-wasm-guest-tinygo/handler"
 	"github.com/http-wasm/http-wasm-guest-tinygo/handler/api"
 )
+
+// Config the plugin configuration.
+type Config struct {
+	Headers map[string]string `json:"headers,omitempty"`
+}
 
 func main() {
 	var config Config
@@ -29,49 +30,14 @@ func main() {
 	handler.HandleRequestFn = mw.handleRequest
 }
 
-// Config the plugin configuration.
-type Config struct {
-	Headers map[string]string `json:"headers,omitempty"`
-}
-
 // Demo a Demo plugin.
-type Demo struct {
-	headers  map[string]string
-	template *template.Template
-}
+type Demo struct{}
 
 // New created a new Demo plugin.
 func New(config Config) (*Demo, error) {
-	if len(config.Headers) == 0 {
-		return nil, fmt.Errorf("headers cannot be empty")
-	}
-
-	return &Demo{
-		headers:  config.Headers,
-		template: template.New("demo").Delims("[[", "]]"),
-	}, nil
+	return &Demo{}, nil
 }
 
-func (a *Demo) handleRequest(req api.Request, resp api.Response) (next bool, reqCtx uint32) {
-	for key, value := range a.headers {
-		tmpl, err := a.template.Parse(value)
-		if err != nil {
-			resp.SetStatusCode(http.StatusInternalServerError)
-			resp.Body().Write([]byte(err.Error()))
-			return false, 0
-		}
-
-		writer := &bytes.Buffer{}
-
-		err = tmpl.Execute(writer, req)
-		if err != nil {
-			resp.SetStatusCode(http.StatusInternalServerError)
-			resp.Body().Write([]byte(err.Error()))
-			return false, 0
-		}
-
-		req.Headers().Set(key, writer.String())
-	}
-
+func (d Demo) handleRequest(req api.Request, resp api.Response) (next bool, reqCtx uint32) {
 	return true, 0
 }
